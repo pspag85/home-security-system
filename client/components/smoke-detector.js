800b961b529da0c59f432f4e9b146902a9c6f20a
pspag85 <pspag85@gmail.com>
2019-04-23 01:01:05 -0400

    feat: trigger flood

--- client/components/smoke-detector.js
+++ client/components/smoke-detector.js
@@ -1,27 +1,53 @@
 import React, {Component} from 'react'
 
 class SmokeDetector extends Component {
-  state = {
-    smoke: false
+  constructor(props) {
+    super(props)
+    this.state = {
+      sprinklersRunning: false,
+      flooding: false
+    }
+    this.sprinklerRef = React.createRef()
+    this.floodRef = React.createRef()
   }
 
   turnOff = () => {
     this.setState({
-      smoke: false
+      sprinklersRunning: false
     })
   }
 
+  triggerFlood = () => {
+    this.floodRef.current.style.display = 'block'
+    this.setState({
+      flooding: true
+    })
+  }
+
+  triggerSprinklers = () => {
+    setTimeout(() => {
+      this.sprinklerRef.current.style.display = 'block'
+      this.triggerFlood()
+    }, 3000)
+  }
+
   render() {
-    const {turnOff, triggerSprinklers} = this
+    const {turnOff, triggerSprinklers, sprinklerRef, floodRef} = this
     const {smoke} = this.props
+    if(smoke) triggerSprinklers()
     return smoke ? (
       <div>
+        <div id='sprinkler-alert'
+          ref={sprinklerRef}>
+          Fire sprinklers running. Turn off in 3 seconds to prevent flooding!
+        </div>
         <div id='smoke-detector'>
           <h3>Smoke Detector</h3>
           <button onClick={turnOff}>
             OFF
           </button>
         </div>
+      <div id='flood' ref={floodRef}></div>
       </div>
     ):(
       <div>
